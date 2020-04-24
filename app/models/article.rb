62f6821a9dadd1842edeed331d414ff2879c595e
Crisel Ayala <criseljazminal@ufps.edu.co>
2020-04-24 16:28:50 -0500

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy 
   # hacer la relación 1 a muchos con el modelo conmentarios
-  # dependent: :destroy -> cuando se elimina un artículo, también se eliminan sus comentarios
+  # dependent: :destroy -> Eliminación en cascada: cuando se elimina un artículo, también se eliminan sus comentarios
 
   # validar los datos que se reciben de los formularios de crear y editar
   # validar que mínimo el artículo debe tener un título y debe ser mayor a 5 caracteres
